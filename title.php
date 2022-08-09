93d45c16dae2a92c2a0fe9cbd431d281e0f3e13c
Nawasan <arikato110011@gmail.com>
2022-08-09 21:57:03 +0700

    change title()

--- title.php
+++ title.php
@@ -1,8 +1,4 @@
 <?php
 return function ($title) {
-    if(isset($GLOBALS['title'])) {
         $GLOBALS['title'] = $title;
-    } else {
-        echo '<script>alert("$GLOBAL[\'title\'] is not set")</script>';
-    }
 };
